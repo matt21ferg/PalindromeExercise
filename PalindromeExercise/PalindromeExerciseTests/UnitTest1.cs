80d00f700d0b4dd662ec66b6a37fb1eedba9042c
matt21ferg <matt21ferguson@gmail.com>
2022-02-02 13:17:06 -0600

    done

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak",true)]
+        [InlineData("tenet", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("Kayak", true)]
+        [InlineData("poop",true)]
+        [InlineData("Poop", true)]
+        [InlineData("hello",false)]
+        [InlineData("matt",false)]
+        [InlineData("pizza",false)]
+        public void PalindromeTest(string word ,bool expected)
         {
-
+            var wordsmith = new WordSmith();
+            var actual = wordsmith.IsAPalindrome(word);
+               Assert.Equal(expected, actual);
         }
     }
 }
